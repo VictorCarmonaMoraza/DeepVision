bbb043f13315c724a08b75936568c2733a495c17
Victor Carmona <victorcarmona9035.com>
2025-08-26 14:31:16 +0200

    Agregar informacion al swagger

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
 from api import predict
 
-app = FastAPI(title="DeepVision API")
+app = FastAPI(
+    title="DeepVision API",
+    description="""
+    🚀 API for DeepVision Project.  
+    Upload an image and get predictions with class labels and confidence levels.  
+
+    ## Endpoints
+    - `/predict`: Upload an image and get the predicted label.  
+    """,
+    version="1.0.0",
+    contact={
+        "name": "Victor M",
+        "url": "https://www.linkedin.com/in/tu-perfil",
+        "email": "tuemail@example.com",
+    },
+    license_info={
+        "name": "MIT License",
+    },
+)
 
 # Incluir rutas
 app.include_router(predict.router)
